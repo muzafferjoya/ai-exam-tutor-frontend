26766212fe089f2d7cd31a3a5bf4081b41fcfc9e
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2025-09-01 16:18:20 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://ai-exam-tutor-api.onrender.com'
+const API_URL = import.meta.env.VITE_API_URL || 'http://13.217.103.47:8000'
 
 const api = axios.create({
   baseURL: API_URL
