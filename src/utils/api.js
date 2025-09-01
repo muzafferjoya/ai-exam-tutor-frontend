bd67d88780f14e3242071cebeb7b9b343325c562
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2025-09-01 17:03:59 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 const API_URL = import.meta.env.VITE_API_URL || 'http://13.217.103.47:8000'
 
 const api = axios.create({
-  baseURL: API_URL
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 })
 
 export default api
