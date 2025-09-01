a48235e9f6028095e07e408df61b5189ccc9381a
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2025-09-01 18:30:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Onboarding from './pages/Onboarding'
 import Dashboard from './pages/Dashboard'
+import Notes from './pages/Notes'
+import Quiz from './pages/Quiz'
+import Progress from './pages/Progress'
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Onboarding />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/notes" element={<Notes />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/progress" element={<Progress />} />
         </Routes>
       </div>
     </BrowserRouter>
